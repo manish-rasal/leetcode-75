ba9399d49593063b89e7f6ee2cd6a9b4fd332903
manishrasal <manishrasal2000@gmail.com>
2025-04-07 17:51:12 +0530

    Monotonic stack

---
+++ monotonic-stack/739. Daily Temperatures/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
